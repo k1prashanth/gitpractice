87cab436fd466a6bb39569b50afb22e7817f6574
k1prashanth <k.prashanth7770@gmail.com>
2018-07-31 12:32:54 +0000

    sample program to for adding two integers

--- ocm.java
+++ ocm.java
@@ -1,5 +1,14 @@
-1 switching to branches
+public class ocm
 {
 
-	ksjdfhkdfnksd
+    public static void main(String[] args) 
+    {
+        
+        int first = 10;
+        int second = 20;
+
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
 }
